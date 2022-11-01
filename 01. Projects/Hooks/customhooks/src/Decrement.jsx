32e9601fb4a8f8fcefed90b5455ab1aece89c504
Ericawanja <wanjaerica@gmail.com>
2022-11-02 00:59:48 +0300

    custome hook

--- 01. Projects/Hooks/customhooks/src/Decrement.jsx
+++ 01. Projects/Hooks/customhooks/src/Decrement.jsx
@@ -1,20 +1,23 @@
 import React from "react";
 import { useEffect } from "react";
+import useCounterChange from "./UseCounterChange";
 
 function Decrement({ setCounter }) {
-  let interval;
-  const handleDecrement = () => {
-    interval = setInterval(() => {
-      setCounter((prev) => prev - 1);
-    }, 1000);
-  };
-  useEffect(() => {
-    handleDecrement();
-    return () => {
-      setCounter(0);
-      clearInterval(interval);
-    };
-  }, []);
+    let action = 'minus'
+    useCounterChange (action, setCounter)
+//   let interval;
+//   const handleDecrement = () => {
+//     interval = setInterval(() => {
+//       setCounter((prev) => prev - 1);
+//     }, 1000);
+//   };
+//   useEffect(() => {
+//     handleDecrement();
+//     return () => {
+//       setCounter(0);
+//       clearInterval(interval);
+//     };
+//   }, []);
   return <div>p</div>;
 }
 
--- 01. Projects/Hooks/customhooks/src/Icrement.jsx
+++ 01. Projects/Hooks/customhooks/src/Icrement.jsx
@@ -1,21 +1,23 @@
 import React from "react";
 import { useEffect } from "react";
 import { useState } from "react";
-
+import useCounterChange from "./UseCounterChange";
 function Icrement({ setCounter }) {
-  let interval;
-  const handleIncrement = () => {
-    interval = setInterval(() => {
-      setCounter((prev) => prev + 1);
-    }, 1000);
-  };
-  useEffect(() => {
-    handleIncrement();
-    return () => {
-        setCounter(0)
-      clearInterval(interval);
-    };
-  }, []);
+    let action = 'add'
+    useCounterChange (action, setCounter)
+//   let interval;
+//   const handleIncrement = () => {
+//     interval = setInterval(() => {
+//       setCounter((prev) => prev + 1);
+//     }, 1000);
+//   };
+//   useEffect(() => {
+//     handleIncrement();
+//     return () => {
+//         setCounter(0)
+//       clearInterval(interval);
+//     };
+//   }, []);
   return <div></div>;
 }
 
